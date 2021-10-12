dd2fe1104546e2ccc8570cb6e63e08088c537e67
abhilashreddys <45734454+abhilashreddys@users.noreply.github.com>
2021-10-12 20:49:13 +0530

    debug check

--- src/data/DatasetReader.py
+++ src/data/DatasetReader.py
@@ -77,6 +77,7 @@ class DatasetReader(object):
         # Prepare for PET MLM objective
         if "PET_MLM" in type:
             if(self.config.cmlm):
+                print("Using Conditional MLM")
                 return self.dataset_reader.prepare_pet_cmlm_batch(batch, mode=type.replace("PET_MLM_", ""))
             return self.dataset_reader.prepare_pet_mlm_batch(batch, mode=type.replace("PET_MLM_", ""))
         # Prepare for evaluation objective
