05c1c2bc08904ea9548d1d41b3d37062868b7e40
abhilashreddys <45734454+abhilashreddys@users.noreply.github.com>
2021-12-23 01:14:27 +0530

    save predictions in txt file

--- src/test.py
+++ src/test.py
@@ -26,6 +26,7 @@ if __name__ == "__main__":
     model = adapet(config, tokenizer, dataset_reader).to(device)
     model.load_state_dict(torch.load(os.path.join(args.exp_dir, "epoch_"+str(config.epochs-1)+"_"+config.pretrained_weight+"_best_model.pt")))
     # test_eval(config, model, batcher)
-    test_alpha_acc, _ = test_alpha_eval(config, model, batcher,alpha=int(args.alpha))
+    test_alpha_acc, test_alpha_logits = test_alpha_eval(config, model, batcher,alpha=int(args.alpha))
     print("Test alpha_%s Acc: %.3f" % (args.alpha,test_alpha_acc) + '\n')
+    np.savetxt(os.path.join(args.exp_dir, 'test_alpha'+str(args.alpha)+'.txt'),np.argmax(test_alpha_logits,axis=1))
 
