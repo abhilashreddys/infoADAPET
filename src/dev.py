0781f97c73ea83f060c68c05125f413c40d9aa65
abhilashreddys <45734454+abhilashreddys@users.noreply.github.com>
2022-01-26 16:04:51 +0530

    save dev logits

--- src/dev.py
+++ src/dev.py
@@ -13,6 +13,7 @@ from src.utils.util import device
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-e', "--exp_dir", required=True)
+    parser.add_argument('-d', "--dataset", required=True,default="")
     args = parser.parse_args()
 
     config_file = os.path.join(args.exp_dir, "config.json")
@@ -25,9 +26,10 @@ if __name__ == "__main__":
     dataset_reader = batcher.get_dataset_reader()
     
     model = adapet(config, tokenizer, dataset_reader).to(device)
-    model.load_state_dict(torch.load(os.path.join(args.exp_dir, "best_model.pt")))
+    model.load_state_dict(torch.load(os.path.join(args.exp_dir, "epoch_"+str(config.epochs-1)+"_"+config.pretrained_weight+"_best_model.pt")))
     dev_acc, dev_logits = dev_eval(config, model, batcher, 0)
 
     print("Dev Acc: %.3f" % (dev_acc) + '\n')
+    np.savetxt(os.path.join(config.exp_dir, 'dev_'+str(args.dataset)+'.txt'),np.argmax(dev_logits,axis=1))
     with open(os.path.join(config.exp_dir, "dev_logits.npy"), 'wb') as f:
         np.save(f, dev_logits)
