80db1a18426053d6f7dccd4125c89eb763a68004
abhilashreddys <45734454+abhilashreddys@users.noreply.github.com>
2022-03-19 18:39:02 +0530

    support for bpr

--- src/data/DatasetReader.py
+++ src/data/DatasetReader.py
@@ -34,6 +34,8 @@ class DatasetReader(object):
             self.dataset_reader = RTEReader(self.config, tokenizer)
         elif self.dataset.lower() == "infotabs/drr":
             self.dataset_reader = infotabsReader(self.config, tokenizer)
+        elif self.dataset.lower() == "infotabs/bpr":
+            self.dataset_reader = infotabsReader(self.config, tokenizer)
         elif self.dataset.lower() == "infotabs/linear_notok":
             self.dataset_reader = infotabsReader(self.config, tokenizer)
         elif self.dataset.lower() == "infotabs/linear_tok":
